76b005e0ecec6c8289fb84908cb6df575967ca67
Gilles Chehade <gilles@poolp.org>
2018-09-04 23:05:02 +0200

    do not go to lka if there are no filters anyways

--- smtpd/smtp_session.c
+++ smtpd/smtp_session.c
@@ -1446,20 +1446,21 @@ smtp_query_filters(struct smtp_session *s, int filter_phase, const char *args)
 	uint8_t	i;
 
 	if (env->sc_smtp_experimental_filter) {
-		m_create(p_lka, IMSG_SMTP_FILTER, 0, 0, -1);
-		m_add_id(p_lka, s->id);
-		m_add_int(p_lka, filter_phase);
-		m_add_string(p_lka, args);
-		m_close(p_lka);
-		tree_xset(&wait_filters, s->id, s);
-		return;
+		if (TAILQ_FIRST(&env->sc_filter_rules[filter_phase])) {
+			m_create(p_lka, IMSG_SMTP_FILTER, 0, 0, -1);
+			m_add_id(p_lka, s->id);
+			m_add_int(p_lka, filter_phase);
+			m_add_string(p_lka, args);
+			m_close(p_lka);
+			tree_xset(&wait_filters, s->id, s);
+			return;
+		}
 	}
 
 	if (filter_phase == FILTER_CONNECTED) {
 		smtp_proceed_connected(s);
 		return;
 	}
-
 	for (i = 0; i < nitems(commands); ++i)
 		if (commands[i].filter_code == filter_phase)
 			commands[i].cb(s, args);
